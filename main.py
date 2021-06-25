d66c7f85600ea774824eba5ceccb123ffeb6a57a
nianny <xiaonianhe@hotmail.com>
2021-06-25 18:08:16 +0800

    ahh fudging

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from firebase_admin import credentials
 cred = credentials.Certificate("firebaseAuth.json")
 firebase_admin.initialize_app(cred)
 
-client = Client(command_prefix="n!", intents=discord.Intents.all(), help_command=None, allowed_mentions=discord.AllowedMentions.all())
+client = Client(command_prefix="h!", intents=discord.Intents.all(), help_command=None, allowed_mentions=discord.AllowedMentions.all())
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('n! help'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('h!help'))
     print("We have logged in as {0.user}".format(client))
 
-client.run(os.getenv('niannybot'))
+client.run(os.getenv('countingBot'))
