d8258ee850da78f9eea0f8fb19a15d85f06ca97b
nianny <xiaonianhe@hotmail.com>
2021-06-29 21:25:33 +0800

    hallooooo

--- cogs/counting.py
+++ cogs/counting.py
@@ -9,6 +9,10 @@ class Counting(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.db = firestore.client()
+        self.data = {}
+        docs = self.db.collection(u'servers').stream()
+        for doc in docs:
+            self.data[doc.id] = doc.to_dict()
     
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -20,21 +24,19 @@ class Counting(commands.Cog):
             string = string.split("_")[0]
             string = string.split("*")[0]
             if string.isdigit():
-                doc_ref = self.db.collection(u'servers').document(u'{}'.format(message.guild.id))
-                doc = doc_ref.get()
-                if not doc.exists:
+                if str(message.guild.id) not in self.data:
                     dict2 = {
                         "count": 0,
                         "lastuser": None,
                         "highestcount": 0
                     }
                 else:
-                    dict2 = doc.to_dict()
+                    dict2 = self.data[str(message.guild.id)]
                 num = int(string)
                 if not (num == dict2["count"]+1):
                     dict2["count"] = 0
                     dict2["lastuser"] = None
-                    doc_ref.set(dict2)
+                    self.data[str(message.guild.id)] = dict2
                     await message.add_reaction("❌")
                     embed = discord.Embed(
                         title="Wrong Count",
@@ -43,14 +45,13 @@ class Counting(commands.Cog):
                     )
                     await message.channel.send(embed=embed)
                     await message.channel.edit(topic=f"Count: {dict2['count']}")
-                    await asyncio.sleep(2)
                     
                     
                     
                 elif (message.author.id==dict2["lastuser"]):
                     dict2["count"] = 0
                     dict2["lastuser"] = None
-                    doc_ref.set(dict2)
+                    self.data[str(message.guild.id)] = dict2
                     await message.add_reaction("❌")
                     embed = discord.Embed(
                         title="You cannot count twice in a row",
@@ -59,31 +60,21 @@ class Counting(commands.Cog):
                     )
                     await message.channel.send(embed=embed)
                     await message.channel.edit(topic=f"Count: {dict2['count']}")
-                    await asyncio.sleep(2)
                     
                 else:
                     if (num > dict2["highestcount"]):
                         dict2["highestcount"] = num
-                        doc_ref.set(dict2)
+                        self.data[str(message.guild.id)] = dict2
                         await message.add_reaction("☑️")
                         await message.channel.edit(topic=f"Count: {dict2['count']}")
                     else:
                         dict2["count"] = num
                         dict2["lastuser"] = message.author.id
-                        doc_ref.set(dict2)
+                        self.data[str(message.guild.id)] = dict2
                         await message.add_reaction("✅")
                         await message.channel.edit(topic=f"Count: {dict2['count']}")
-                    
 
-                # with concurrent.futures.ThreadPoolExecutor() as executer:
-                #     f1 = executer.submit(doc_ref.set, dict2)
-                #     f2 = executer.submit(message.channel.edit, topic=f"hallo")
-                
-                # print(dict2["count"])
-
-                 
-                # print(string)
-
+                self.db.collection(u'servers').document(str(message.guild.id)).set(dict2)
 
 def setup(client):
     client.add_cog(Counting(client))
